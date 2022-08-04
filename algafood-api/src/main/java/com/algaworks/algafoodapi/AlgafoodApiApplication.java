733c69babb066c4d7c80d02cb044195cfb6bdf78
Rayane Maciel <rayanemaciel10@gmail.com>
2022-08-04 13:41:20 -0300

    Iniciando projeto

---
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/AlgafoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgafoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgafoodApiApplication.class, args);
+	}
+
+}
---
+++ algafood-api/src/test/java/com/algaworks/algafoodapi/AlgafoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafoodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
