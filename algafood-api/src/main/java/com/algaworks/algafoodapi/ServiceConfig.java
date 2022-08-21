8f7addd6243a6c114f43a56f60ec06716aad6a91
Rayane <rayanemaciel10@gmail.com>
2022-08-20 23:20:03 -0300

    Ciclo de vida dos beans e callback

--- algafood-api/src/main/java/com/algaworks/algafoodapi/ServiceConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/ServiceConfig.java
@@ -1,12 +1,17 @@
 package com.algaworks.algafoodapi;
 
+import com.algaworks.algafoodapi.di.service.AtivacaoClienteService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ServiceConfig {
 
-//    @Bean
-//    public AtivacaoClienteService ativacaoClienteService(Notificador notificador){
-//        return new AtivacaoClienteService(notificador);
-//    }
+
+    @Bean(initMethod = "init", destroyMethod = "destroy")
+    public AtivacaoClienteService ativacaoClienteService(){
+        return new AtivacaoClienteService();
+    }
+
+
 }
--- algafood-api/src/main/java/com/algaworks/algafoodapi/di/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/di/service/AtivacaoClienteService.java
@@ -7,6 +7,8 @@ import com.algaworks.algafoodapi.di.notificacao.TipoDoNotificador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 
 @Component
@@ -15,6 +17,16 @@ public class AtivacaoClienteService {
     @TipoDoNotificador(NivelUrgencia.URGENTE)
     @Autowired
     private Notificador notificador;
+
+    @PostConstruct
+    public void init(){
+        System.out.println("INIT");
+    }
+
+    @PreDestroy
+    public void destroy(){
+        System.out.println("\n DESTROY!!!!!!!!!!");
+    }
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
